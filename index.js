60a86b69aa4f12d73b4453ef5e2fc12922c51001
Robel Haftom <robelhaftom80@gmail.com>
2023-07-12 16:38:31 +0300

    the play feature has been added

--- index.js
+++ index.js
@@ -2,16 +2,28 @@ const timerEl = document.getElementById("timer");
 const startButton = document.getElementById("start-button");
 const stopButton = document.getElementById("stop-button");
 const resetButton = document.getElementById("reset-button");
+const playButton = document.getElementById("play-button");
 
 let startTime = 0;
 let elapsedTime = 0;
 let timeInterval;
+let playing = false;
 
 startButton.addEventListener("click", startTimer);
 
 stopButton.addEventListener("click", stopTimer);
 
 resetButton.addEventListener("click",resetTimer);
+
+playButton.addEventListener("click",()=>{
+    if(playing){
+        clearInterval(timeInterval);
+        playing = false;
+    } else{
+        playStartTimer();
+        playing = true;
+    }
+});
 
 function startTimer(){
     startTime = Date.now();
@@ -53,4 +65,18 @@ function formatTime(elapsedTime){
          "." +
         (milliseconds > 9 ? milliseconds : "0" + milliseconds)
         );
+}
+
+function playStartTimer(){
+    startTime = Date.now();
+    timeInterval = setInterval(() => {
+        elapsedTime = Date.now() - startTime;
+        timerEl.textContent = formatPlayTime(elapsedTime);
+    }, 10);
+}
+
+function formatPlayTime(){
+    const milliseconds = Math.floor((elapsedTime
+        % 1000) / 10);
+    return milliseconds > 9 ? milliseconds : "0" + milliseconds;
 }
